408bf180c17732c2a7cb644b2bc07fddabc48b0b
Etrejos97 <149202132+Etrejos97@users.noreply.github.com>
2026-05-22 16:00:48 -0500

    mover migraciones y superusuario al wsgi, simplificar buildCommand

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -4,4 +4,19 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
+
+from django.core.management import call_command
+try:
+    call_command('migrate', '--run-syncdb', verbosity=0)
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(email='admin@admin.com').exists():
+        User.objects.create_superuser(
+            email='admin@admin.com',
+            password='Admin1234!',
+            username='admin'
+        )
+except Exception:
+    pass
+
 app = application
