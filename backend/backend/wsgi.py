0f4f64bb9325ae51090e452f00120fcdd2da3c59
Etrejos97 <149202132+Etrejos97@users.noreply.github.com>
2026-05-22 16:06:28 -0500

    Deploy limpio

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -13,7 +13,7 @@ try:
     if not User.objects.filter(email='admin@admin.com').exists():
         User.objects.create_superuser(
             email='admin@admin.com',
-            password='Admin1234!',
+            password='admin',
             username='admin'
         )
 except Exception:
