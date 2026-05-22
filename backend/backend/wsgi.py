4d6f2c7f2e54ac59d42c67eebed0ae33c11739e8
Etrejos97 <149202132+Etrejos97@users.noreply.github.com>
2026-05-22 15:43:00 -0500

    revertir wsgi.py y agregar buildCommand en vercel.json

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -3,16 +3,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-if os.environ.get('VERCEL'):
-    from django.db import connection
-    from django.db.migrations.executor import MigrationExecutor
-    try:
-        executor = MigrationExecutor(connection)
-        if executor.migration_plan(executor.loader.graph.leaf_nodes()):
-            from django.core.management import call_command
-            call_command('migrate', '--run-syncdb')
-    except Exception:
-        pass
-
 application = get_wsgi_application()
 app = application
