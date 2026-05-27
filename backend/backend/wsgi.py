bdbfc3cedf065b8251f9e3585f097a700a65b85b
Etrejos97 <149202132+Etrejos97@users.noreply.github.com>
2026-05-27 15:17:43 -0500

    fix: limpiar wsgi.py de migraciones automáticas y corregir build_files.sh

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -5,19 +5,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
 
-from django.core.management import call_command
-try:
-    call_command('migrate', '--run-syncdb', verbosity=0)
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-    if not User.objects.filter(email='admin@admin.com').exists():
-        User.objects.create_superuser(
-            email='admin@admin.com',
-            password='admin.',
-            username='admin',
-            role='admin'
-        )
-except Exception:
-    pass
-
 app = application
