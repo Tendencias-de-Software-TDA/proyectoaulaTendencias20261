aa8d2838c26d71144462646cd0a2e381689ee731
Etrejos97 <149202132+Etrejos97@users.noreply.github.com>
2026-05-26 20:54:08 -0500

    cambio superuser

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -13,7 +13,7 @@ try:
     if not User.objects.filter(email='admin@admin.com').exists():
         User.objects.create_superuser(
             email='admin@admin.com',
-            password='admin',
+            password='admin.',
             username='admin',
             role='admin'
         )
--- backend/build_files.sh
+++ backend/build_files.sh
@@ -6,7 +6,7 @@ python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(email='admin@admin.com').exists():
-    User.objects.create_superuser(email='admin@admin.com', password='admin', username='admin')
+    User.objects.create_superuser(email='admin@admin.com', password='admin.', username='admin')
     print('Superusuario creado')
 else:
     print('Superusuario ya existe')
